7a638057a84f6da3296eb8d3576181530b2a24bd
Kaloyan Lambov <85471593+KBLambov20@users.noreply.github.com>
2023-04-03 15:02:49 +0300

    Fix backspace functionality. Close #20
    Backspace deletes full element instead of separate chars

--- HIP/_build/calculator.cpp
+++ HIP/_build/calculator.cpp
@@ -7,12 +7,13 @@ void displayCalculate(Texture2D CalcBackground) {
 	DrawTextureEx(CalcBackground, { 0, 0 }, 0, 1, WHITE);
 }
 
-
+// add elements to the text box
 void addElement(Vector2 coordinates) {
 
 	if (textIndex >= 25) {
 		return;
 	}
+
 	if (coordinates.x == 79 && coordinates.y == 439) {
 		equationText[textIndex++] = 'M';
 		equationText[textIndex++] = 'g';
@@ -48,17 +49,14 @@ void addElement(Vector2 coordinates) {
 		equationText[textIndex++] = 'A';
 		equationText[textIndex++] = 'l';
 	}
+	else if (coordinates.x == 1718 && coordinates.y == 583) {
+		equationText[textIndex++] = 'O';
+	}
 	else if (coordinates.x == 1719 && coordinates.y == 438) {
 		equationText[textIndex++] = 'H';
-		equationText[textIndex++] = '2';
 	}
 	else if (coordinates.x == 1582 && coordinates.y == 438) {
 		equationText[textIndex++] = 'N';
-		equationText[textIndex++] = '2';
-	}
-	else if (coordinates.x == 1718 && coordinates.y == 583) {
-		equationText[textIndex++] = 'O';
-		equationText[textIndex++] = '2';
 	}
 	else if (coordinates.x == 1582 && coordinates.y == 583) {
 		equationText[textIndex++] = 'S';
@@ -66,7 +64,6 @@ void addElement(Vector2 coordinates) {
 	else if (coordinates.x == 1444 && coordinates.y == 583) {
 		equationText[textIndex++] = 'C';
 		equationText[textIndex++] = 'l';
-		equationText[textIndex++] = '2';
 	}
 	else if (coordinates.x == 1308 && coordinates.y == 584) {
 		equationText[textIndex++] = 'P';
@@ -74,11 +71,9 @@ void addElement(Vector2 coordinates) {
 	else if (coordinates.x == 1445 && coordinates.y == 743) {
 		equationText[textIndex++] = 'B';
 		equationText[textIndex++] = 'r';
-		equationText[textIndex++] = '2';
 	}
 	else if (coordinates.x == 1308 && coordinates.y == 743) {
 		equationText[textIndex++] = 'I';
-		equationText[textIndex++] = '2';
 	}
 	else if (coordinates.x == 1172 && coordinates.y == 742) {
 		equationText[textIndex++] = 'C';
@@ -110,24 +105,54 @@ void addElement(Vector2 coordinates) {
 	else if (coordinates.x == 906 && coordinates.y == 629) {
 		equationText[textIndex++] = '0';
 	}
-	else if (coordinates.x == 789 && coordinates.y == 629) {
-		equationText[textIndex++] = '(';
-	}
-	else if (coordinates.x == 1024 && coordinates.y == 628) {
-		equationText[textIndex++] = ')';
-	}
-	else if (coordinates.x == 911 && coordinates.y == 703) {
-		equationText[textIndex++] = '+';
-	}
 	else if (coordinates.x == 789 && coordinates.y == 389) {
 		equationText[textIndex++] = '1';
 	}
-	else if (coordinates.x == 1591 && coordinates.y == 164) {
-		textIndex--;
-		if (textIndex < 0) {
-			textIndex = 0;
+	else if (textIndex != 0 && equationText[textIndex-1] != '+' && equationText[textIndex - 1] != '>') {
+		if (coordinates.x == 789 && coordinates.y == 629) {
+			equationText[textIndex++] = '(';
 		}
-		equationText[textIndex] = '\0';
+		else if (coordinates.x == 1024 && coordinates.y == 628) {
+			equationText[textIndex++] = ')';
+		}
+		else if (coordinates.x == 911 && coordinates.y == 703) {
+			equationText[textIndex++] = '+';
+		}
+		else if (coordinates.x == 909 && coordinates.y == 785) {
+			equationText[textIndex++] = '-';
+			equationText[textIndex++] = '>';
+		}
+	}
+
+	if (coordinates.x == 1591 && coordinates.y == 164) {	
+		switch (equationText[textIndex - 1]) {
+			case 'g':
+			case 'a':
+			case 'e':
+			case 'u':
+			case 'l':
+			case 'r':
+			case '>':
+				if (textIndex - 2 >= 0) {
+					textIndex--;
+					for (int i = textIndex; i > textIndex - 2; i--) {
+						equationText[i] = '\0';
+					}
+					textIndex--;
+				}
+				else {
+					textIndex = 0;
+				}
+
+				break;
+			default:
+				if (--textIndex < 0) {
+					textIndex = 0;
+				}
+				equationText[textIndex] = '\0';
+
+				break;
+		}
 	}
 	
 }
@@ -136,22 +161,21 @@ void displayTextBox() {
 }
 
 void displayElements(Texture2D metals, Texture2D nonmetals, Texture2D metalsButtonsHover, Texture2D nonmetalsButtonsHover) {
-	string metalElementSign = "";
 
 	for (int i = 1; i <= 9; i++)
 	{
 		double specialWidth = 0;
 		Vector2 coordinates = { 0, 0 };
 		switch (i) {
-		case 1:	specialWidth = (metals.width / 9.1); coordinates = { 79, 439 }; metalElementSign = "Mg"; break;
-		case 2:	specialWidth = (metals.width / 9.1); coordinates = { 215, 439 }; metalElementSign = "Na"; break;
-		case 3: specialWidth = (metals.width / 9.1); coordinates = { 79, 584 }; metalElementSign = "Ca"; break;
-		case 4: specialWidth = (metals.width / 9 - 0.1); coordinates = { 215.5, 584 }; metalElementSign = "K"; break;
-		case 5: specialWidth = (metals.width / 9); coordinates = { 353, 584 }; metalElementSign = "Fe"; break;
-		case 6: specialWidth = (metals.width / 9.1 + 1); coordinates = { 491, 584 }; metalElementSign = "Cu"; break;
-		case 7: specialWidth = (metals.width / 9.1 + 1.2); coordinates = { 354, 743 }; metalElementSign = "Ag"; break;
-		case 8: specialWidth = (metals.width / 9.1 + 1.3); coordinates = { 490, 743 }; metalElementSign = "Au"; break;
-		case 9: specialWidth = (metals.width / 9.1 + 1.1); coordinates = { 629, 742 }; metalElementSign = "Al"; break;
+		case 1:	specialWidth = (metals.width / 9.1); coordinates = { 79, 439 };break;
+		case 2:	specialWidth = (metals.width / 9.1); coordinates = { 215, 439 }; break;
+		case 3: specialWidth = (metals.width / 9.1); coordinates = { 79, 584 }; break;
+		case 4: specialWidth = (metals.width / 9 - 0.1); coordinates = { 215.5, 584 }; break;
+		case 5: specialWidth = (metals.width / 9); coordinates = { 353, 584 }; break;
+		case 6: specialWidth = (metals.width / 9.1 + 1); coordinates = { 491, 584 }; break;
+		case 7: specialWidth = (metals.width / 9.1 + 1.2); coordinates = { 354, 743 }; break;
+		case 8: specialWidth = (metals.width / 9.1 + 1.3); coordinates = { 490, 743 }; break;
+		case 9: specialWidth = (metals.width / 9.1 + 1.1); coordinates = { 629, 742 }; break;
 		default: break;
 		}
 		Rectangle elementFrame = { i * specialWidth, 0, (float)(metals.width / 9.1), (float)(metals.height) };
@@ -170,22 +194,22 @@ void displayElements(Texture2D metals, Texture2D nonmetals, Texture2D metalsButt
 		}
 
 	}
-	string nonmetalElementSign = "";
+
 	for (int i = 1; i <= 9; i++)
 	{
 		float specialWidth = 0;
 		Vector2 coordinates = { 0, 0 };
 		switch (i) {
-		case 1:	specialWidth = (nonmetals.width / 9.1 + 0.1); coordinates = { 1719, 438 }; nonmetalElementSign = "H2"; break;
-		case 2:	specialWidth = (nonmetals.width / 9.1 + 0.1); coordinates = { 1582, 438 }; nonmetalElementSign = "N2"; break;
-		case 3: specialWidth = (nonmetals.width / 9.1 + 1); coordinates = { 1718, 583 }; nonmetalElementSign = "O2"; break;
-		case 4: specialWidth = (metals.width / 9 - 0.1); coordinates = { 1582, 583 }; nonmetalElementSign = "S"; break;
-		case 5: specialWidth = (nonmetals.width / 9 + 1); coordinates = { 1444, 583 }; nonmetalElementSign = "Cl2"; break;
-		case 6: specialWidth = (nonmetals.width / 9.1 + 1.3); coordinates = { 1308, 584 }; nonmetalElementSign = "P"; break;
-		case 7: specialWidth = (nonmetals.width / 9.1 + 1.3); coordinates = { 1445, 743 }; nonmetalElementSign = "Br2"; break;
-		case 8: specialWidth = (nonmetals.width / 9.1 + 1.2); coordinates = { 1308, 743 }; nonmetalElementSign = "I2"; break;
-		case 9: specialWidth = (nonmetals.width / 9.1 + 1.1); coordinates = { 1172, 742 }; nonmetalElementSign = "C"; break;
-		default: break;
+			case 1:	specialWidth = (nonmetals.width / 9.1 + 0.1); coordinates = { 1719, 438 }; break;
+			case 2:	specialWidth = (nonmetals.width / 9.1 + 0.1); coordinates = { 1582, 438 }; break;
+			case 3: specialWidth = (nonmetals.width / 9.1 + 1); coordinates = { 1718, 583 }; break;
+			case 4: specialWidth = (metals.width / 9 - 0.1); coordinates = { 1582, 583 }; break;
+			case 5: specialWidth = (nonmetals.width / 9 + 1); coordinates = { 1444, 583 }; break;
+			case 6: specialWidth = (nonmetals.width / 9.1 + 1.3); coordinates = { 1308, 584 }; break;
+			case 7: specialWidth = (nonmetals.width / 9.1 + 1.3); coordinates = { 1445, 743 }; break;
+			case 8: specialWidth = (nonmetals.width / 9.1 + 1.2); coordinates = { 1308, 743 }; break;
+			case 9: specialWidth = (nonmetals.width / 9.1 + 1.1); coordinates = { 1172, 742 }; break;
+			default: break;
 		}
 		Rectangle elementFrame = { i * specialWidth, 0, (float)(metals.width / 9.1), (float)(metals.height) };
 		float x = coordinates.x;
